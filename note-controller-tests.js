944f24bcd6bbcfb2ee507217521f17081b9962de
Harry LB <harry.lingardbright@gmail.com>
2020-08-21 17:08:33 +0100

    refactored tests

--- note-controller-tests.js
+++ note-controller-tests.js
@@ -9,17 +9,6 @@
   }
   renderListAfterSubmit();
 
-  function singleNoteViewTest() {
-    var list = new NoteList();
-    list.addNote('Favourite Drink: Seltzer');
-    var controller = new NoteController(list);
-    controller.makeUrlChangeShowCurrentNote(controller.view);
-    window.location.hash = '#notes/0';
-    window.dispatchEvent(new HashChangeEvent("hashchange"));
-    assert.isTrue(document.getElementById('app').innerHTML === "<div>Favourite Drink: Seltzer</div>");
-  }
-  singleNoteViewTest(); 
-
   function renderEmptyList() {
     var list = new NoteList();
     var controller = new NoteController(list);
