bdbb4bb8f03eda0cabb2bfd27074ab3d96d047c2
Harry LB <harry.lingardbright@gmail.com>
2020-08-21 17:29:44 +0100

    specrunner refactor

---
+++ note-add-tests.js
@@ -0,0 +1,11 @@
+(function(exports) {
+  function renderListAfterSubmit(){
+    var list = new NoteList();
+    var controller = new NoteController(list);
+    controller.listenForSubmit();
+    document.getElementById('textfield').value="This is a test!";
+    document.dispatchEvent(new Event("submit"));
+    assert.isTrue(document.getElementById('app').innerHTML === '<ul><li><div><a href="#notes/0">This is a test!</a></div></li></ul>');
+  }
+  renderListAfterSubmit();
+})(this)
--- note-controller-tests.js
+++ note-controller-tests.js
@@ -1,13 +1,14 @@
 (function(exports) {
-  function renderListAfterSubmit(){
+  function singleNoteViewTest() {
     var list = new NoteList();
+    list.addNote('Favourite Drink: Seltzer');
     var controller = new NoteController(list);
-    controller.listenForSubmit();
-    document.getElementById('textfield').value="This is a test!";
-    document.dispatchEvent(new Event("submit"));
-    assert.isTrue(document.getElementById('app').innerHTML === '<ul><li><div><a href="#notes/0">This is a test!</a></div></li></ul>');
+    controller.makeUrlChangeShowCurrentNote(controller.view);
+    window.location.hash = '#notes/0';
+    window.dispatchEvent(new HashChangeEvent("hashchange"));
+    assert.isTrue(document.getElementById('app').innerHTML === "<div>Favourite Drink: Seltzer</div>");
   }
-  renderListAfterSubmit();
+  singleNoteViewTest(); 
 
   function renderEmptyList() {
     var list = new NoteList();
